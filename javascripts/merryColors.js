0c7cd9e49815b28344c3ebf9d95efa61f2654448
simonhfrost <simonhfrost@gmail.com>
2015-06-20 01:23:28 +0200

    use complimentary color for highlighted element

--- javascripts/merryColors.js
+++ javascripts/merryColors.js
@@ -3,8 +3,11 @@
 
 var randomColor = Please.make_color()[0]
 
+var scheme = Please.make_scheme(Please.HEX_to_HSV(randomColor), { scheme_type: 'complementary' })
+var opposite = scheme[scheme.length - 1]
+
 var MerryColors = {
-  ACTIVE_COLOR: new THREE.MeshLambertMaterial({color: new THREE.Color(0xFFFFFF)}),
+  ACTIVE_COLOR: new THREE.MeshLambertMaterial({color: new THREE.Color(opposite)}),
   INACTIVE_COLOR: new THREE.MeshLambertMaterial({color: randomColor }),
   HIGHLIGHTED_COLOR: new THREE.MeshLambertMaterial({color: new THREE.Color(0x666666)})
 }
