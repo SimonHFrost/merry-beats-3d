2e9dceb42a51d1b144b7acedf9fa972e61950667
simonhfrost <simonhfrost@gmail.com>
2015-06-20 00:29:29 +0200

    get random color for cubes

--- javascripts/merryColors.js
+++ javascripts/merryColors.js
@@ -1,7 +1,10 @@
 /* global THREE */
+/* global Please */
+
+var randomColor = Please.make_color()[0]
 
 var MerryColors = {
   ACTIVE_COLOR: new THREE.MeshLambertMaterial({color: new THREE.Color(0xFFFFFF)}),
-  INACTIVE_COLOR: new THREE.MeshLambertMaterial({color: new THREE.Color(0x444444)}),
+  INACTIVE_COLOR: new THREE.MeshLambertMaterial({color: randomColor }),
   HIGHLIGHTED_COLOR: new THREE.MeshLambertMaterial({color: new THREE.Color(0x666666)})
 }
