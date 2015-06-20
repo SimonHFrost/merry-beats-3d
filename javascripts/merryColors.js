de4bd510268b125df382774a5f85fb50c15cc649
simonhfrost <simonhfrost@gmail.com>
2015-06-20 19:36:05 +0200

    color theme buttons

--- javascripts/merryColors.js
+++ javascripts/merryColors.js
@@ -10,6 +10,13 @@ var third = scheme[2]
 
 document.body.style.backgroundColor = third
 
+document.getElementsByTagName('button')[0].style.backgroundColor = first
+document.getElementsByTagName('button')[0].style.color = 'white'
+document.getElementsByTagName('button')[1].style.backgroundColor = first
+document.getElementsByTagName('button')[1].style.color = 'white'
+document.getElementsByTagName('button')[2].style.backgroundColor = first
+document.getElementsByTagName('button')[2].style.color = 'white'
+
 var MerryColors = {
   ACTIVE_COLOR: new THREE.MeshLambertMaterial({color: new THREE.Color(second)}),
   INACTIVE_COLOR: new THREE.MeshLambertMaterial({color: new THREE.Color(first)}),
