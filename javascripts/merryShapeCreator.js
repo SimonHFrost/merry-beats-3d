98c491a0fab498334d156857ecde71d18005c649
simonhfrost <simonhfrost@gmail.com>
2015-07-19 20:35:51 +0200

    add information about rotation bug

--- javascripts/merryShapeCreator.js
+++ javascripts/merryShapeCreator.js
@@ -14,10 +14,10 @@ MerryShapeCreator.prototype.createShape = function (x, z, shape) {
     return this._createMesh(new THREE.BoxGeometry(0.25, 0.25, 0.75), x, z)
   } else if (shape === 'slanted') {
     var mesh = this._createMesh(new THREE.BoxGeometry(0.25, 0.25, 0.75), x, z)
-    mesh.rotation.y += 10
+    mesh.rotation.y += 40 * (Math.PI / 180)
     return mesh
   } else {
-    console.warn('Unknown shape: ' + shape)
+    console.warn('Can not create unknown shape: ' + shape)
   }
 }
 
@@ -29,7 +29,12 @@ MerryShapeCreator.prototype._createMesh = function (geometry, x, z) {
   mesh.position.y = 0
   mesh.position.z = z
 
+  // FIXME: The current incorrect up vector flips the lookat function after 180 degrees
+  //        See https://github.com/mrdoob/three.js/issues/1460
+  mesh.up.set(0, 1, 0)
   mesh.lookAt(new THREE.Vector3(0, 0, 0))
+
+  console.log(mesh.rotation.y * 180 / Math.PI)
 
   this.scene.add(mesh)
   return mesh
